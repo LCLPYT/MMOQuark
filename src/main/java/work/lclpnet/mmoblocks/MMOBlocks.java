7e79c6241997e7527d80591c90bdf2880953f955
DerBerliner <34981166+DerBerlinr@users.noreply.github.com>
2021-09-30 21:51:40 +0200

    added iron_plate and rusty_iron_plate (including stairs,slabs)

--- src/main/java/work/lclpnet/mmoblocks/MMOBlocks.java
+++ src/main/java/work/lclpnet/mmoblocks/MMOBlocks.java
@@ -3,6 +3,7 @@ package work.lclpnet.mmoblocks;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Blocks;
+import work.lclpnet.mmoblocks.block.MMOBlock;
 import work.lclpnet.mmoblocks.block.MMOBlockRegistrar;
 
 public class MMOBlocks implements ModInitializer {
@@ -15,5 +16,9 @@ public class MMOBlocks implements ModInitializer {
 				.withSlab().withStairs().withWall().register("weeping_blackstone_bricks");
 		new MMOBlockRegistrar(AbstractBlock.Settings.copy(Blocks.NETHER_BRICKS))
 				.withSlab().withStairs().withWall().register("charred_nether_bricks");
+		new MMOBlockRegistrar(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK))
+				.withSlab().withStairs().register("iron_plate");
+		new MMOBlockRegistrar(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK))
+				.withSlab().withStairs().register("rusty_iron_plate");
 	}
 }
