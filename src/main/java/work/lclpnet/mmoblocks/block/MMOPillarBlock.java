fea7e81c5502ed6c4a671f437bee56a4709f2113
erikh <wertist.2000@gmail.com>
2021-10-30 20:05:15 +0200

    added Myalite Blocks

---
+++ src/main/java/work/lclpnet/mmoblocks/block/MMOPillarBlock.java
@@ -0,0 +1,17 @@
+package work.lclpnet.mmoblocks.block;
+
+import net.minecraft.block.PillarBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class MMOPillarBlock extends PillarBlock implements IMMOBlock {
+
+    public MMOPillarBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public BlockItem provideBlockItem(Item.Settings settings) {
+        return new BlockItem(this, settings);
+    }
+}
